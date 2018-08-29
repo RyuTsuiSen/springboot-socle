a1796723273a29ad3fb7f7f8b299d36d6b904c51
S641883 <S641883@UC504768VW7JAV.siege.axa-fr.intraxa>
2018-08-29 14:00:31 +0200

    update absolute path handlerchain

--- springboot-socle-soap/src/main/java/fr/trandutrieu/remy/socle/soap/WebserviceImpl.java
+++ springboot-socle-soap/src/main/java/fr/trandutrieu/remy/socle/soap/WebserviceImpl.java
@@ -2,7 +2,7 @@ package fr.trandutrieu.remy.socle.soap;
 
 import javax.jws.HandlerChain;
 
-@HandlerChain(file = "../../../../../../handlers.xml")
+@HandlerChain(file = "/handlers.xml")
 public abstract class WebserviceImpl implements Webservice {
 
 }
