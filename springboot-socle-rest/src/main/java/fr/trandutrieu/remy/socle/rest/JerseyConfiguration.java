b7b0281570f22a9aeb8fcbb91695712c9c493b33
rtrandut <rtdt@gmail.com>
2020-11-06 19:19:03 +0100

    module api

--- springboot-socle-rest/src/main/java/fr/trandutrieu/remy/socle/rest/JerseyConfiguration.java
+++ springboot-socle-rest/src/main/java/fr/trandutrieu/remy/socle/rest/JerseyConfiguration.java
@@ -1,24 +1,19 @@
 package fr.trandutrieu.remy.socle.rest;
 
-import javax.annotation.PostConstruct;
-import javax.ws.rs.ApplicationPath;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
 import fr.trandutrieu.remy.socle.rest.filter.ApplicationResourceFilter;
 import fr.trandutrieu.remy.socle.rest.filter.ProcessAndAuditBusinessExceptionMapper;
 import fr.trandutrieu.remy.socle.rest.filter.ProcessAndAuditErrorMapper;
 import fr.trandutrieu.remy.socle.rest.filter.ProcessAndAuditRuntimeExceptionMapper;
 import fr.trandutrieu.remy.socle.rest.resources.MappersResource;
 import fr.trandutrieu.remy.socle.rest.resources.WhoAmIResource;
+import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("api")
 public abstract class JerseyConfiguration extends ResourceConfig {
 	public JerseyConfiguration() {
 		super();
+		setUp();
 	}
-	
-	@PostConstruct
+
 	public void setUp() {
 		register(ApplicationResourceFilter.class);
 		register(ProcessAndAuditErrorMapper.class);
@@ -27,7 +22,6 @@ public abstract class JerseyConfiguration extends ResourceConfig {
 		register(WhoAmIResource.class);
 		register(MappersResource.class);
 		init();
-		
 	}
 
 	public abstract void init();
--- springboot-socle-rest/src/main/java/fr/trandutrieu/remy/socle/rest/springboot/ApplicationRestAbstract.java
+++ springboot-socle-rest/src/main/java/fr/trandutrieu/remy/socle/rest/springboot/ApplicationRestAbstract.java
@@ -1,25 +1,21 @@
 package fr.trandutrieu.remy.socle.rest.springboot;
 
-import java.util.Arrays;
-
+import com.netflix.config.ConfigurationManager;
+import fr.trandutrieu.remy.socle.AdapterServlet;
 import org.apache.commons.configuration.AbstractConfiguration;
 import org.apache.commons.configuration.event.ConfigurationEvent;
 import org.apache.commons.configuration.event.ConfigurationListener;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
-import com.netflix.config.ConfigurationManager;
-
-import fr.trandutrieu.remy.socle.AdapterServlet;
-import fr.trandutrieu.remy.socle.rest.resources.MappersResource;
-import fr.trandutrieu.remy.socle.rest.resources.WhoAmIResource;
+import java.util.Arrays;
 
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {MappersResource.class, WhoAmIResource.class})
 public abstract class ApplicationRestAbstract {
 
 	static {
@@ -37,7 +33,11 @@ public abstract class ApplicationRestAbstract {
 			}
 		}); 
 	}
-	
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> contextRoot() {
+		return factory -> factory.setContextPath("/eaas-st-rest-api");
+	}
 	
     @Bean
     public ServletRegistrationBean<AdapterServlet> myServletRegistration () {
